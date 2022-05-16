84647de24caabd06cad204e7c2bb31fb05dd8c06
Graham Ashton <graham@effectif.com>
2022-05-16 17:39:14 +0100

    Return all trips from GET /api/trips

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -1,9 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+import prisma from "lib/prisma"
+
+export default async function handler(req, res) {
   if (req.method !== 'GET') {
     res.status(405).json({ message: 'Method Not Allowed' })
     return
   }
-  res.status(200).json({ test: 'Trippy fucker' })
+  const trips = await prisma.trip.findMany()
+  res.status(200).json(trips)
 }
