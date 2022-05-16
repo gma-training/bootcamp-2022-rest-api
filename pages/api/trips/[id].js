766d1ec1ddd9c3ae4f0103e5ecfc969c9be0d812
Graham Ashton <graham@effectif.com>
2022-05-16 21:12:25 +0100

    Implement PUT /api/trips/:id

--- pages/api/trips/[id].js
+++ pages/api/trips/[id].js
@@ -10,7 +10,20 @@ export default async function handler(req, res) {
       if (!trip) {
         return res.status(404).json({ message: "Not Found" })
       }
-      return res.status(200).json(trip)
+
+      res.status(200).json(trip)
+      break
+    }
+
+    case "PUT": {
+      const { user, name, start_date, end_date } = req.body
+      await prisma.trip.update({
+        where: { id: parseInt(id) },
+        data: { user, name, start_date, end_date }
+      })
+
+      res.status(200).end()
+      break
     }
   }
 }
