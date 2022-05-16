59558df619a00d5c54b0a8d32a16d026688f82e7
Graham Ashton <graham@effectif.com>
2022-05-16 19:56:36 +0100

    Implement POST /api/trips

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -3,10 +3,28 @@
 import prisma from "lib/prisma"
 
 export default async function handler(req, res) {
-  if (req.method !== 'GET') {
-    res.status(405).json({ message: 'Method Not Allowed' })
-    return
+  switch (req.method) {
+    case "GET": {
+      const trips = await prisma.trip.findMany()
+      res.status(200).json(trips)
+      break
+    }
+    case "POST": {
+      const { user, name, start_date, end_date } = req.body
+      if (!user) {
+        return res.status(400).json({ message: "`user` is required" })
+      }
+      if (!name) {
+        return res.status(400).json({ message: "`name` is required" })
+      }
+
+      await prisma.trip.create({ data: { user, name, start_date, end_date } })
+
+      res.status(200).end()
+      break
+    }
+    default: {
+      res.status(405).json({ message: 'Method Not Allowed' })
+    }
   }
-  const trips = await prisma.trip.findMany()
-  res.status(200).json(trips)
 }
